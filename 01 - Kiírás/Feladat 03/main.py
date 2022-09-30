341f8607e1352c5e1f7e8751d3eb9a5105bbcae9
Matedor <k6mate@gmail.com>
2022-09-30 07:48:45 +0200

    initail commit

---
+++ 01 - Kiírás/Feladat 03/main.py
@@ -0,0 +1,6 @@
+dynasty:str="Ming"
+firstYear:int=1368
+secondYear:int=1644
+wallName:str="Kínai Nagyfal"
+lenght:int=7200
+print(f"A {dynasty} dinasztia {firstYear} és {secondYear} közt építette a ma látható {wallName} {lenght}km hosszú szakaszát.")
