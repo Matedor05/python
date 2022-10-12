0f0a5d23f0b2ff2f35e1be1bc43d247b0872f4fb
Matedor05 <k6mate@gmail.com>
2022-10-12 07:17:44 +0200

    feladat

---
+++ 01 - Kiírás/Feladat 05/main.py
@@ -0,0 +1,6 @@
+showYear:int=2008
+manufracturer:str="Sukhoj"
+type:str="SU-35S"
+speed:int=2.4
+unit:str="m/s"
+print(f"Oroszország legmodernebb vadászrepülőgépe a {showYear}-banbemutatott {manufracturer} gyártotta {type} amely képes a {speed} Mach sebességre, ami 720{unit}-nak felel meg")
