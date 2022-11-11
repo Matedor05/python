f8c655aa4abc621b00ebaabdf2d34dfd1131eab8
Matedor05 <k6mate@gmail.com>
2022-11-11 07:32:58 +0100

    javítás

--- 05 - Elágazások (if-else)/Feladat 10/main.py
+++ 05 - Elágazások (if-else)/Feladat 10/main.py
@@ -5,9 +5,9 @@ num1=int(input())
 
 if(num1% 3==0 and num1% 2==0):
     print("ZIZI")
-elif(num1% 2==0 and num1% 3!=0):
+elif(num1% 2==0):
     print("BIZ")
-elif(num1% 3==0 and num1% 2!=0):
+elif(num1% 3==0):
     print("BAZ")
 else:
     print(":(")
--- 05 - Elágazások (if-else)/Feladat 11/main.py
+++ 05 - Elágazások (if-else)/Feladat 11/main.py
@@ -5,17 +5,17 @@ num1=int(input())
 
 if(num1 % 2 == 0):
     print("A szám páros")
-elif(num1 % 2 != 0):
+else:
     print("A szám páratlan")
 
 if(num1>=1):
     print("A szám pozitív")
 elif(num1<=-1):
     print("A szám negatív")
-elif(num==0):
+elif(num1==0):
     print("A szám a 0")
 
 if(num1 %5 ==0):
     print("a szam osztható 5-tel")
-elif(num1 %5 !=0):
+else:
     print("a szam nem osztható 5-tel")
