fcc5e268f834d3fd0bef86265bd6c7a45bea654c
Karasz Máté <b1karmat@vasvari.org>
2023-05-05 07:53:41 +0200

    q

--- 11 - OOP/4 Feladat/computer.py
+++ 11 - OOP/4 Feladat/computer.py
@@ -1,6 +1,7 @@
 from videokartya import Videokartya
 from processzor import Processzor
 from tápegység import Tapegyseg
+
 class Pc():
     def __init__(self,videokartya:Videokartya,processzor:Processzor,tapegyseg:Tapegyseg)->None:
         super().__init__()
@@ -9,6 +10,5 @@ class Pc():
         self.processzor = processzor
         self.tapegyseg = tapegyseg
 
-
     def __str__(self) -> str:
-        return f"Tulajdonos: {self.tulajdonos}\n\nVideokártya:\nGyártó: {self.videokartya.gyarto}\nTípus: {self.videokartya.tipus}\nFogyasztás: {self.videokartya.fogyasztas}\n\nProcesszor:\nGyártó:{self.processzor.gyarto}\nTípus: {self.processzor.tipus}\nFogyasztás: {self.processzor.fogyasztas}\n\nTápegység:\nGyártó:{self.tapegyseg.gyarto}\nTípus: {self.tapegyseg.tipus}\nFogyasztás: {self.tapegyseg.teljesitmeny}"
+        return f"Tulajdonos: {self.tulajdonos}\n\nVideokártya:\nGyártó: {self.videokartya.gyarto}\nTípus: {self.videokartya.tipus}\nFogyasztás: {self.videokartya.fogyasztas}\n\nProcesszor:\nGyártó:{self.processzor.gyarto}\nTípus: {self.processzor.tipus}\nFogyasztás: {self.processzor.fogyasztas}\n\nTápegység:\nGyártó:{self.tapegyseg.gyarto}\nTípus: {self.tapegyseg.tipus}\nTeljesítméy: {self.tapegyseg.teljesitmeny}"
