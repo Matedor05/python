23d68cdbd8233fe0826183960bbb9933e29307b9
Karasz Máté <b1karmat@vasvari.org>
2023-05-19 07:49:22 +0200

    rossz

--- 11 - OOP/5 feladat/main.py
+++ 11 - OOP/5 feladat/main.py
@@ -5,7 +5,7 @@ tolltarto: Tolltarto = Tolltarto()
 tolltarto.radir = "Koh-i-noor 300/60"
 tolltarto.ceruza = "Spirit: Deluxe"
 tolltarto.tollakSzama = "5"
-tolltarto.hibajavító = "UNI CLP-300"
+tolltarto.hibajavito = "UNI CLP-300"
 tolltarto.toll = "SIGNETTA"
 
 
