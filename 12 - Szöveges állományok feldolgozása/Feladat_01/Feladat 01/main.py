a408491dd38b225d0dbe4ac5c251739717131549
Karasz Máté <b1karmat@vasvari.org>
2023-05-23 08:36:41 +0200

    nehéz

---
+++ 12 - Szöveges állományok feldolgozása/Feladat_01/Feladat 01/main.py
@@ -0,0 +1,37 @@
+from typing import *
+from student import Student
+from studentIO import *
+from services import *
+
+
+# 1 - Írjuk ki minden diák adatát a képernyőre!
+students: List[Student] = importStudents()
+print("Az osztaly tanulói: ")
+for student in students:
+    print(student)
+
+# 2 - Hány diák jár az osztályba?
+countOfClass: int = len(students)
+print(f"Az osztályba {countOfClass} tanuló jár\n\n")
+
+# 3 - Mennyi az osztály átlaga?
+classAvarage: float = calculateAvarage(students)
+print(f"Az osztaly atlaga: {classAvarage:1.2f}")
+
+# 4 - Keressük a legtöbb pontot elért érettségizőt és írjuk ki az adatait a képernyőre.
+bestStudent: Student = getBestStudent(students)
+print(f"Best student is : {bestStudent}")
+
+# 5 - atlagfelett.txt allományba keressük ki azon tanulókat kiknek pontjai meghaladják az átlagot!
+aboveAvarage:List[Student] = studentAboveAvarage(students,classAvarage)
+writeStudentsInFile(aboveAvarage, "atlagfelett.txt")
+
+# 6 - Van e kitünő tanulónk?
+# 7 - Hány elégtelen, elégséges, jó, jeles és kitünő tanuló van az osztályban?
+#     Értékhatárok:
+# 	- elégtelen, ha: 0.00 - 1.99
+# 	- elégséges, ha: 2.00 - 2.99
+# 	- jó, ha: 3.00 - 3.99
+# 	- jeles, ha: 4.00 - 4.99
+	# - kitünő, ha: 5.00
+
---
+++ 12 - Szöveges állományok feldolgozása/Feladat_01/Feladat 01/services.py
@@ -0,0 +1,26 @@
+from typing import *
+from student import Student
+
+
+def calculateAvarage(students: List[Student])->float:
+    sum: float = 0
+    
+    for student in students:
+        sum += student.avarage
+    return sum / len(students)
+
+def getBestStudent(students: List[Student])->Student:
+    bestStudent: Student = students[0] #referencia ertek
+    for index in range(1, len(students),1):
+        if(students[index].avarage > bestStudent.avarage):
+            bestStudent = students[index]
+    return bestStudent
+
+def studentAboveAvarage(students: List[Student],classAvarage:float)->List[Student]:
+    aboveAvarage:List[Student] = []
+
+    for student in students:
+        if(student.avarage>= classAvarage):
+            aboveAvarage.append(student)
+
+    return aboveAvarage
